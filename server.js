0486762e54ba02f3bf814711c7d22a58607c1f3f
Laila EL HAJJAMY <laila.elhajjamy@dxc.com>
2023-10-23 19:49:34 +0100

    Creating Routes & Responses inExpress

--- server.js
+++ server.js
@@ -5,6 +5,26 @@ const dotenv = require("dotenv");
 
 dotenv.config({ path: "./config/config.env" });
 const app = express();
+app.get("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, msg: "Show all bootcamps" });
+});
+
+app.post("/api/v1/bootcamps", (req, res) => {
+  res.status(201).json({ success: true, msg: "Bootcamp successfully created" });
+});
+
+app.put("/api/v1/bootcamp/:id", (req, res) => {
+  res
+    .status(200)
+    .json({
+      success: true,
+      msg: `bootcamp updated successfully ${req.params.id}`,
+    });
+});
+
+app.delete("/api/v1/bootcamp/:id", (req, res) => {
+  res.status(200).json({ success: true, msg: "Bootcamp deleted successfully" });
+});
 const PORT = process.env.PORT || 5000;
 app.listen(
   PORT,
