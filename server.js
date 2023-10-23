2b189dca5d85e4241d89221f4b15c6891513546b
Laila EL HAJJAMY <laila.elhajjamy@dxc.com>
2023-10-23 20:56:44 +0100

    morgan middleware

--- server.js
+++ server.js
@@ -8,8 +8,11 @@ const bootcamps = require("./routes/bootcamps");
 
 dotenv.config({ path: "./config/config.env" });
 const app = express();
-const logger = require("./middleware/logger");
-app.use(logger);
+const morgan = require("morgan");
+// Dev logging middleware
+if (process.env.MODE_ENV == "development") {
+  app.use(morgan("dev"));
+}
 app.use("/api/v1/bootcamps", bootcamps);
 const PORT = process.env.PORT || 5000;
 app.listen(
