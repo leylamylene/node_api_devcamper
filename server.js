12774690f30b2c1faac7180d5b748fca3e2f312c
Laila EL HAJJAMY <laila.elhajjamy@dxc.com>
2023-10-23 19:26:29 +0100

    set up env

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+// load dot env vars
+
+dotenv.config({ path: "./config/config.env" });
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.listen(
+  PORT,
+  console.log(
+    `Server running  in ${process.env.MODE_ENV} MODE IN PORT ${process.env.PORT}`
+  )
+);
