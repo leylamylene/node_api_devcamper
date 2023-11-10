b4e916a84ec5e8fd67df1c2a47db5ce9861c9f72
Laila EL HAJJAMY <laila.elhajjamy@dxc.com>
2023-11-10 15:29:19 +0100

    return token with register

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ dotenv.config({ path: "./config/config.env" });
 
 const bootcamps = require("./routes/bootcamps");
 const courses = require("./routes/courses");
+const auth = require("./routes/auth");
 // load dot env vars
 
 connectDb();
@@ -33,6 +34,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // Mount routes
 app.use("/api/v1/bootcamps", bootcamps);
 app.use("/api/v1/courses", courses);
+app.use("/api/v1/auth", auth);
 
 app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
