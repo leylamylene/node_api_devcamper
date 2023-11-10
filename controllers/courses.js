609b074b337f96e348cf49ca7c047797829f782c
Laila EL HAJJAMY <laila.elhajjamy@dxc.com>
2023-11-10 14:36:37 +0100

    courses advanced results refactoring

--- controllers/courses.js
+++ controllers/courses.js
@@ -12,24 +12,15 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
   let query;
 
   if (req.params.bootcampId) {
-    query = Course.find({ bootcamp: req.params.bootcampId });
-  } else {
-    query = Course.find().populate({
-      path: "bootcamp",
-      select: "name description",
+    const courses = await Course.find({ bootcamp: req.params.bootcampId });
+    return res.status(200).json({
+      success: true,
+      count: courses.lenght,
+      data: courses,
     });
+  } else {
+    res.status(200).json(res.advancedResults);
   }
-
-  const courses = await query;
-  if (!courses) {
-    return next(
-      new ErrorResponse(
-        `courses not found for bootcamp with id of ${req.params.bootcampId}`,
-        404
-      )
-    );
-  }
-  res.status(200).json({ success: true, count: courses.length, data: courses });
 });
 
 //@desc Get a single course
