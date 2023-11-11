a015bdfe1a77ab9ded281fe52cee6dc920a5b72f
Laila EL HAJJAMY <laila.elhajjamy@dxc.com>
2023-11-11 01:36:27 +0100

    authorization

--- middleware/auth.js
+++ middleware/auth.js
@@ -15,7 +15,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
   // Make sure token exists
 
   if (!token) {
-    return next(new ErrorResponse("Not authorized to access this route"));
+    return next(new ErrorResponse("Not authorized to access this route", 401));
   }
 
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
@@ -24,7 +24,7 @@ exports.protect = asyncHandler(async (req, res, next) => {
   req.user = await User.findById(decoded.id);
 
   if (!req.user) {
-    return next(new ErrorResponse("Not authorized to access this route"));
+    return next(new ErrorResponse("Not authorized to access this route", 401));
   }
   next();
 
@@ -32,3 +32,19 @@ exports.protect = asyncHandler(async (req, res, next) => {
   //   token = req.cookies.token
   // }
 });
+
+// Grant access to specific roles
+
+exports.authorize = (...roles) => {
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new ErrorResponse(
+          `user role ${req.user.role} is not authorized to access this route`,
+          403
+        )
+      );
+    }
+    next();
+  };
+};
